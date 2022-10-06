cac965a50517e5a523f1a3530a8b1d6671f1309a
Jonathan <ghuyers98@gmail.com>
2022-10-06 17:59:52 +0800

    add color picker attribute and start stroke width indicator

--- javascript/color-picker.js
+++ javascript/color-picker.js
@@ -1,15 +1,15 @@
 const strokeColorPicker = document.querySelector(".stroke-color-picker");
 const fillColorPicker = document.querySelector(".fill-color-picker");
 
-const startingColor = "black";
-strokeColorPicker.style.backgroundColor = startingColor;
-fillColorPicker.style.backgroundColor = startingColor;
+strokeColorPicker.setAttribute("selected-color", "black");
+fillColorPicker.setAttribute("selected-color", "black");
 
 const strokePopup = new Picker({
     parent: strokeColorPicker,
     color: "black",
     onChange: function(color) {
         strokeColorPicker.style.backgroundColor = color.rgbaString;
+        strokeColorPicker.setAttribute("selected-color", color.rgbaString);
     }
 });
 
@@ -18,5 +18,6 @@ const fillPopup = new Picker({
     color: "black",
     onChange: function(color) {
         fillColorPicker.style.backgroundColor = color.rgbaString;
+        fillColorPicker.setAttribute("selected-color", color.rgbaString);
     }
 });
