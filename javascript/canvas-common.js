588e4a21e32261d5a60672280b03f6e6bc8ac493
Jonathan <ghuyers98@gmail.com>
2022-10-07 16:03:31 +0800

    add button selected feature

--- javascript/canvas-common.js
+++ javascript/canvas-common.js
@@ -1,6 +1,5 @@
 const canvasReal = document.querySelector("#canvas-real");
 const canvasDraft = document.querySelector("#canvas-draft");
-
 const contextReal = canvasReal.getContext("2d");
 const contextDraft = canvasDraft.getContext("2d");
 let currentFunction;
@@ -16,3 +15,59 @@ const polygonButton = document.querySelector(".polygon-button");
 const bezierCurveButton = document.querySelector(".bezier-curve-button");
 const textButton = document.querySelector(".text-button");
 const eraserButton = document.querySelector(".eraser-button");
+
+drawingToolButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let currentSelectedButton = document.querySelector("[selected=true]");
+        currentSelectedButton.setAttribute("selected", "false");
+        button.setAttribute("selected", "true");
+    })
+})
+
+canvasDraft.addEventListener("mousedown", (event) => {
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
+    currentFunction.onMouseDown([mouseX, mouseY], event);
+    dragging = true;
+})
+
+canvasDraft.addEventListener("mousemove", (event) => {
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
+    
+    if(dragging) {
+        currentFunction.onDragging([mouseX, mouseY], event);
+    }
+
+    currentFunction.onMouseMove([mouseX,mouseY], event);
+})
+
+canvasDraft.addEventListener("mouseup", (event) => {
+    dragging = false;
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
+    currentFunction.onMouseUp([mouseX, mouseY], event);
+})
+
+canvasDraft.addEventListener("mouseleave", (event) => {
+    dragging = false;
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
+    currentFunction.onMouseDown([mouseX, mouseY], event);
+})
+
+canvasDraft.addEventListener("mouseenter", (event) => {
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
+    currentFunction.onMouseEnter([mouseX, mouseY], event);
+})
+
+class PaintFunction {
+    constructor() {}
+    onMouseDown() {}
+    onDragging() {}
+    onMouseMove() {}
+    onMouseUp() {}
+    onMouseLeave() {}
+    onMouseEnter() {}
+}
