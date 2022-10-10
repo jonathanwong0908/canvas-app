bccf6f12824606cd8805afe6e6a98aff61cc04d8
Jonathan <ghuyers98@gmail.com>
2022-10-10 18:18:37 +0800

    update undo redo js

--- javascript/undo-redo.js
+++ javascript/undo-redo.js
@@ -32,30 +32,3 @@ function updateCanvasHistory() {
     canvasHistoryArray[canvasHistoryIndex] = contextReal.getImageData(0, 0, canvasReal.width, canvasReal.height);
     canvasHistoryIndex++;
 }
-
-// function undo() {
-//     if (canvasHistoryIndex === 0) {
-//         clearCanvas();
-//         // from clear-canvas.js
-//         canvasHistoryIndex--;
-//     } else if (canvasHistoryIndex > 0) {
-//         canvasHistoryIndex--;
-//         contextReal.putImageData(canvasHistoryArray[canvasHistoryIndex], 0, 0);
-//     } else {
-//         return;
-//     }
-// }
-
-// function redo() {
-//     if (canvasHistoryIndex < canvasHistoryArray.length - 1) {
-//         canvasHistoryIndex++;
-//         contextReal.putImageData(canvasHistoryArray[canvasHistoryIndex], 0, 0);
-//     } else {
-//         return;
-//     }
-// }
-
-// function updateCanvasHistory() {
-//     canvasHistoryArray.push(contextReal.getImageData(0, 0, canvasReal.width, canvasReal.height));
-//     canvasHistoryIndex++;
-// }
